678fc96bb9c66abb7701a51ba0e4888b77cebf9b
livaha； <1192137406@qq.com>
2019-11-15 19:17:21 +0800

    深入使用less样式

--- src/layout/Frame.js
+++ src/layout/Frame.js
@@ -2,13 +2,10 @@ import React from 'react';
 import { Layout } from 'antd';
 import Nav from './Nav';
 import '../assets/style/layout.less'
-const { Header, Footer, Sider, Content } = Layout;
-
+const { Sider, Content } = Layout;
 export default function Frame(props) {
   return (<div>
     <Layout className="layout">
-      {/*<Header className="header">Header</Header>*/}
-
       <Layout>
         <Content className="content">
           {props.children}
@@ -17,9 +14,6 @@ export default function Frame(props) {
             <Nav/>
         </Sider>
       </Layout>
-      
-      {/*<Footer className="footer">Footer</Footer>*/}
-      </Layout>
-
+    </Layout>
   </div>)
 }
